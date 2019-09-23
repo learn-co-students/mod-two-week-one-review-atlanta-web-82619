6299a2ec2b78330961dc5af26449b3e14c241c37
Justina Kuchta <justyna.kuchta@gmail.com>
2019-09-23 13:40:12 -0400

    Done

--- Rails-Seven-Seas/app/controllers/seas_controller.rb
+++ Rails-Seven-Seas/app/controllers/seas_controller.rb
@@ -48,9 +48,7 @@ class SeasController < ApplicationController
   end
 
   private
-  # In controller actions, use this private method to access sea params from forms.
-  # Example: @sea.update(sea_params)
-  # check the return value of sea_params to see what you're working with!
+  
   def sea_params
     params.permit(:name, :temperature, :bio, :mood, :image_url, :favorite_color, :scariest_creature, :has_mermaids)
   end
